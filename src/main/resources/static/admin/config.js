09cd0b4f32848c682835170be1a9fd19ef9ebf32
卢德坤 <1364978779@qq.com>
2023-03-27 00:26:06 +0800

    danmai

--- src/main/resources/static/admin/config.js
+++ src/main/resources/static/admin/config.js
@@ -4,8 +4,9 @@
  * @Email: 1364978779@qq.com
  * @Date: 2023-03-01 05:45:20
  * @LastEditors: dekun lu
- * @LastEditTime: 2023-03-03 22:31:59
+ * @LastEditTime: 2023-03-26 04:57:07
  */
 export default {
     API_URL: window.location.origin
+    // API_URL: 'http://localhost:8084'
 }
